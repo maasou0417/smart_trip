0f114eb1014b60cc24e5e49726226d2e9cbed540
Maa Alsamaa Souaidan <maaswidan5@gmail.com>
2025-11-28 15:31:46 +0100

    Updated Files

--- backend/src/server.ts
+++ backend/src/server.ts
@@ -8,6 +8,14 @@ const app = express();
 const PORT = process.env.PORT || 5000;
 
 // Middleware 
+
+app.use(
+  cors({
+    origin: "http://localhost:5173", // Vite frontend
+    credentials: true,
+  })
+);
+
 app.use(cors());
 app.use(express.json());
 app.use(express.urlencoded({ extended: true })); // Extra säkerhet
